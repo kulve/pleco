60230637b1e6bbbf6a8f153568250ed37ff991cb
Tuomas Kulve <tuomas@kulve.fi>
2017-06-29 11:09:23 +0000

    netrelay: Fix indentation

--- netrelay/netrelay.c
+++ netrelay/netrelay.c
@@ -2,7 +2,7 @@
  * netrelay.c: Listen data from a client and a server and send it to
  * each other
  *
- * Copyright 2012 Tuomas Kulve, <tuomas.kulve@snowcap.fi>
+ * Copyright 2012-2017 Tuomas Kulve, <tuomas.kulve@snowcap.fi>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -68,159 +68,159 @@ main(int argc, char **argv)
   /* Open listening socket for client stream connection */
   client_stream_listen_fd = open_udp_socket(NETRELAY_CLIENT_STREAM_PORT);
   if (client_stream_listen_fd == -1) {
-	fprintf(stderr,
-			"Failed to create client stream listen socket for port %d.\n",
-			NETRELAY_CLIENT_STREAM_PORT);
-	exit(-1);
+    fprintf(stderr,
+            "Failed to create client stream listen socket for port %d.\n",
+            NETRELAY_CLIENT_STREAM_PORT);
+    exit(-1);
   }
 
   /* Open listening socket for server stream connection */
   server_stream_listen_fd = open_udp_socket(NETRELAY_SERVER_STREAM_PORT);
   if (server_stream_listen_fd == -1) {
-	fprintf(stderr,
-			"Failed to create server stream listen socket for port %d.\n",
-			NETRELAY_SERVER_STREAM_PORT);
-	close(client_stream_listen_fd);
-	exit(-1);
+    fprintf(stderr,
+            "Failed to create server stream listen socket for port %d.\n",
+            NETRELAY_SERVER_STREAM_PORT);
+    close(client_stream_listen_fd);
+    exit(-1);
   }
 
   /* Listen for new data */
   while (1) {
     fd_set fds;
     int retval;
-	int max_fd = client_stream_listen_fd;
-	struct in_addr  server_stream_addr;
-	struct in_addr  client_stream_addr;
+    int max_fd = client_stream_listen_fd;
+    struct in_addr  server_stream_addr;
+    struct in_addr  client_stream_addr;
 
-	memset(&server_stream_addr, 0, sizeof(server_stream_addr));
-	memset(&client_stream_addr, 0, sizeof(client_stream_addr));
+    memset(&server_stream_addr, 0, sizeof(server_stream_addr));
+    memset(&client_stream_addr, 0, sizeof(client_stream_addr));
 
     FD_ZERO(&fds);
 
-	/* Add client stream fd for udp packets */
+    /* Add client stream fd for udp packets */
     FD_SET(client_stream_listen_fd, &fds);
 
-	/* Add server stream fd for udp packets */
+    /* Add server stream fd for udp packets */
     FD_SET(server_stream_listen_fd, &fds);
-	if (server_stream_listen_fd > max_fd) {
-	  max_fd = server_stream_listen_fd;
-	}
-
-	/* Add client data fd for new data, if connected */
-	if (client_data_fd > -1) {
-	  FD_SET(client_data_fd, &fds);
-	  if (client_data_fd > max_fd) {
-		max_fd = client_data_fd;
-	  }
-	}
-
-	retval = select(max_fd + 1, &fds, NULL, NULL, NULL);
-    if(retval == -1) {
-	  fprintf(stderr, "Failed to select: %s\n",
-			  strerror(errno));
-	  exit(-1);
+    if (server_stream_listen_fd > max_fd) {
+      max_fd = server_stream_listen_fd;
     }
 
-	if(retval == 0) {
-	  printf( "Select timeout (shouldn't happen)\n");
-	  continue;
-	}
+    /* Add client data fd for new data, if connected */
+    if (client_data_fd > -1) {
+      FD_SET(client_data_fd, &fds);
+      if (client_data_fd > max_fd) {
+        max_fd = client_data_fd;
+      }
+    }
 
-	/*printf("New data in %d sockets\n", retval);*/
+    retval = select(max_fd + 1, &fds, NULL, NULL, NULL);
+    if(retval == -1) {
+      fprintf(stderr, "Failed to select: %s\n",
+              strerror(errno));
+      exit(-1);
+    }
 
-	/* Check which socket has new data */
+    if(retval == 0) {
+      printf( "Select timeout (shouldn't happen)\n");
+      continue;
+    }
 
-	/* New client stream data */
-	if (FD_ISSET(client_stream_listen_fd, &fds)) {
-	  char buf[4096];
-	  ssize_t bytes_recv;
+    /*printf("New data in %d sockets\n", retval);*/
 
-	  printf("data from client_stream_listen_fd\n");
+    /* Check which socket has new data */
 
-	  /* Get data from client */
-	  bytes_recv = recvfrom(client_stream_listen_fd, buf, 4096, 0,
-							(struct sockaddr *)&si_client, &slen_client);
-	  if (bytes_recv == -1) {
-		fprintf(stderr, "Failed to receive UDP data from client: %s\n",
-				strerror(errno));
-	  } else {
+    /* New client stream data */
+    if (FD_ISSET(client_stream_listen_fd, &fds)) {
+      char buf[4096];
+      ssize_t bytes_recv;
 
-		client_stream_addr = si_client.sin_addr;
-		client_stream_port = si_client.sin_port;
+      printf("data from client_stream_listen_fd\n");
 
-		printf("Received server packet from %s:%d\n",
-			   inet_ntoa(client_stream_addr), ntohs(client_stream_port));
+      /* Get data from client */
+      bytes_recv = recvfrom(client_stream_listen_fd, buf, 4096, 0,
+                            (struct sockaddr *)&si_client, &slen_client);
+      if (bytes_recv == -1) {
+        fprintf(stderr, "Failed to receive UDP data from client: %s\n",
+                strerror(errno));
+      } else {
 
-		printf("Server port: %d\n", ntohs(server_stream_port));
+        client_stream_addr = si_client.sin_addr;
+        client_stream_port = si_client.sin_port;
 
-		if (ntohs(server_stream_port) > 0) {
-		  ssize_t bytes_sent;
+        printf("Received server packet from %s:%d\n",
+               inet_ntoa(client_stream_addr), ntohs(client_stream_port));
 
-		  printf("Sending client data to %s:%d\n",
-				 inet_ntoa(server_stream_addr), ntohs(server_stream_port));
+        printf("Server port: %d\n", ntohs(server_stream_port));
 
-		  /* Send data to server */
-		  bytes_sent = sendto(server_stream_listen_fd,
-							  buf, (size_t)bytes_recv, 0,
-							  (struct sockaddr *)&si_server, slen_server);
+        if (ntohs(server_stream_port) > 0) {
+          ssize_t bytes_sent;
 
-		  if (bytes_sent < 0) {
-			fprintf(stderr, "Failed to send UDP data to server: %s\n",
-					strerror(errno));
-		  }
+          printf("Sending client data to %s:%d\n",
+                 inet_ntoa(server_stream_addr), ntohs(server_stream_port));
 
-		  if (bytes_sent < bytes_recv) {
-			fprintf(stderr, "Failed to send all UDP data to server: %d < %d\n",
-					(int)bytes_sent, (int)bytes_recv);
-		  }
+          /* Send data to server */
+          bytes_sent = sendto(server_stream_listen_fd,
+                              buf, (size_t)bytes_recv, 0,
+                              (struct sockaddr *)&si_server, slen_server);
 
-		}
-	  }
+          if (bytes_sent < 0) {
+            fprintf(stderr, "Failed to send UDP data to server: %s\n",
+                    strerror(errno));
+          }
 
-	}
+          if (bytes_sent < bytes_recv) {
+            fprintf(stderr, "Failed to send all UDP data to server: %d < %d\n",
+                    (int)bytes_sent, (int)bytes_recv);
+          }
 
-	/* New server stream data */
-	if (FD_ISSET(server_stream_listen_fd, &fds)) {
-	  char buf[4096];
-	  ssize_t bytes_recv;
+        }
+      }
 
-	  printf("data from server_stream_listen_fd\n");
+    }
 
-	  /* Get data from server and send it to client */
-	  bytes_recv = recvfrom(server_stream_listen_fd,
-							buf, 4096, 0,
-							(struct sockaddr *)&si_server, &slen_server);
-	  if ( bytes_recv == -1) {
-		fprintf(stderr, "Failed to receive UDP data from server: %s\n", strerror(errno));
-	  } else {
-		ssize_t bytes_sent;
+    /* New server stream data */
+    if (FD_ISSET(server_stream_listen_fd, &fds)) {
+      char buf[4096];
+      ssize_t bytes_recv;
 
-		server_stream_addr = si_server.sin_addr;
-		server_stream_port = si_server.sin_port;
+      printf("data from server_stream_listen_fd\n");
 
-		printf("Received server packet from %s:%d\n",
-			   inet_ntoa(server_stream_addr), ntohs(server_stream_port));
+      /* Get data from server and send it to client */
+      bytes_recv = recvfrom(server_stream_listen_fd,
+                            buf, 4096, 0,
+                            (struct sockaddr *)&si_server, &slen_server);
+      if ( bytes_recv == -1) {
+        fprintf(stderr, "Failed to receive UDP data from server: %s\n", strerror(errno));
+      } else {
+        ssize_t bytes_sent;
 
-		  /* Send data to client, if we have the address */
-		if (ntohs(client_stream_port) > 0) {
-		  bytes_sent = sendto(client_stream_listen_fd,
-							  buf, (size_t)bytes_recv, 0,
-							  (struct sockaddr *)&si_client, slen_client);
+        server_stream_addr = si_server.sin_addr;
+        server_stream_port = si_server.sin_port;
+
+        printf("Received server packet from %s:%d\n",
+               inet_ntoa(server_stream_addr), ntohs(server_stream_port));
+
+        /* Send data to client, if we have the address */
+        if (ntohs(client_stream_port) > 0) {
+          bytes_sent = sendto(client_stream_listen_fd,
+                              buf, (size_t)bytes_recv, 0,
+                              (struct sockaddr *)&si_client, slen_client);
 		  
-		  if (bytes_sent < 0) {
-			fprintf(stderr, "Failed to send UDP data to client: %s\n",
-					strerror(errno));
-		  }
+          if (bytes_sent < 0) {
+            fprintf(stderr, "Failed to send UDP data to client: %s\n",
+                    strerror(errno));
+          }
 		  
-		  if (bytes_sent < bytes_recv) {
-			fprintf(stderr, "Failed to send all UDP data to client: %d < %d\n",
-					(int)bytes_sent, (int)bytes_recv);
-		  }
-		} else {
-		  fprintf(stderr, "No client port, not sending data to client\n");
-		}
-	  }
-	}
+          if (bytes_sent < bytes_recv) {
+            fprintf(stderr, "Failed to send all UDP data to client: %d < %d\n",
+                    (int)bytes_sent, (int)bytes_recv);
+          }
+        } else {
+          fprintf(stderr, "No client port, not sending data to client\n");
+        }
+      }
+    }
   }
 
   return 0;
@@ -240,14 +240,14 @@ int open_udp_socket(int port)
   /* try to create a socket */
   fd = socket(PF_INET, SOCK_DGRAM, 0);
   if (fd == -1) {
-	fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
-	return -1;
+    fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
+    return -1;
   }
 
   if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof (opt)) < 0) {
-	fprintf(stderr, "Failed to set SO_REUSEADDR: %s\n",
-			strerror(errno));
-	return -1;
+    fprintf(stderr, "Failed to set SO_REUSEADDR: %s\n",
+            strerror(errno));
+    return -1;
   }
 
   /* Clear structure */
@@ -258,13 +258,21 @@ int open_udp_socket(int port)
   addr.sin_addr.s_addr = INADDR_ANY;
 
   if (bind(fd,
-		   (struct sockaddr *) &addr,
-		   sizeof(struct sockaddr_in)) == -1) {
-	fprintf(stderr, "Error binding socket: %s\n", strerror(errno));
-	close(fd);
-	return 0;
+           (struct sockaddr *) &addr,
+           sizeof(struct sockaddr_in)) == -1) {
+    fprintf(stderr, "Error binding socket: %s\n", strerror(errno));
+    close(fd);
+    return 0;
   }
 
   return fd;
 }
 
+
+/* Emacs indentatation information
+   Local Variables:
+   indent-tabs-mode:nil
+   tab-width:2
+   c-basic-offset:2
+   End:
+*/
